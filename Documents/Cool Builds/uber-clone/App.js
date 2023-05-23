96f9548c004928a0157f5e534461a2c136350898
jjb0123 <jjhbrice@gmail.com>
2023-05-22 19:02:18 -0500

    First commit

---
+++ Documents/Cool Builds/uber-clone/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+export default function App() {
+  return (
+    <Provider store={}>
+
+      <View style={styles.container}>
+        <Text>Let's build Uber!</Text>
+          <StatusBar style="auto" />
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
