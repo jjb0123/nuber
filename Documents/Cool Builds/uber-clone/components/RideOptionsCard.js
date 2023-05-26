099fc46419c971f7bc4b3cbfb0e058f95319e2bd
jjb0123 <jjhbrice@gmail.com>
2023-05-26 16:53:42 -0500

    Yeah

--- Documents/Cool Builds/uber-clone/components/RideOptionsCard.js
+++ Documents/Cool Builds/uber-clone/components/RideOptionsCard.js
@@ -4,6 +4,7 @@ import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import { FlatList } from 'react-native';
+import { useState } from 'react';
 
 const data =[
     {
@@ -28,33 +29,48 @@ const data =[
 
 const RideOptionsCard = () => {
     const navigation = useNavigation();
+    const [selected, setSelected] = useState(null);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
         <View>
             <TouchableOpacity 
             onPress= {() => navigation.navigate('NavigateCard')} 
-            style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+            style={tw`absolute top-2 left-5 z-50 p-3 rounded-full`}
             >
                 <Icon name = "chevron-left" type="fontawesome" />
             </TouchableOpacity>
-            <Text style={tw`text-center py-5 text-xl`}>Choose your ride</Text>
+            <Text style={tw`text-center py-4 text-xl`}>Choose your ride</Text>
         </View>
         <FlatList data={data}
         keyExtractor={(item) => item.id}
-            renderItem={({item: { id, title, multiplier, image }}) => (
-            <TouchableOpacity>
+            renderItem={({item: { id, title, multiplier, image }, item}) => (
+            <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+                id === selected?.id && "bg-gray-200"}`}
+            >
                 <Image 
                     style={{
                         width:100,
-                        height:100,
+                        height:90,
                         resizeMode:"contain",
                     }}
                     source = {{ uri: image }}
                 />
+                <View style={tw`-ml-6`}>
+                    <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                    <Text>Travel time...</Text>
+                </View>
+            <Text style={tw`text-xl`}>$99</Text>
             </TouchableOpacity>
             )}
         />
+        <View>
+            <TouchableOpacity disabled ={!selected} style ={tw`bg-black py-3 m-2 rounded-full ${!selected && 'bg-gray-300'}`}>
+                <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+            </TouchableOpacity>
+        </View> 
     </SafeAreaView>
 
   );
