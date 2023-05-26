6f1285ac7a56454cd557c4f3ae5c316a0dc6c0a6
jjb0123 <jjhbrice@gmail.com>
2023-05-26 15:41:13 -0500

    Yes

--- Documents/Cool Builds/uber-clone/components/RideOptionsCard.js
+++ Documents/Cool Builds/uber-clone/components/RideOptionsCard.js
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,TouchableOpacity } from 'react-native';
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import { FlatList } from 'react-native';
+
+const data =[
+    {
+        id:"Uber-X-123",
+        title:"UberX",
+        multiplier:1,
+        image:"https://links.papareact.com/3pn",
+    },
+    {
+        id:"Uber-XL-456",
+        title: "Uber XL",
+        multiplier:1.2,
+        image: "https://links.papareact.com/5w8",
+    },
+    {
+        id: "Uber-LUX-789",
+        title: "Uber LUX",
+        multiplier: 1.75,
+        image: "https://links.papareact.com/7pf",
+    },
+];
 
 const RideOptionsCard = () => {
+    const navigation = useNavigation();
+
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
-      <Text style={tw`text-center py-5 text-xl`}>Choose your ride</Text>
+        <View>
+            <TouchableOpacity 
+            onPress= {() => navigation.navigate('NavigateCard')} 
+            style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+            >
+                <Icon name = "chevron-left" type="fontawesome" />
+            </TouchableOpacity>
+            <Text style={tw`text-center py-5 text-xl`}>Choose your ride</Text>
+        </View>
+        <FlatList 
+
+        />
     </SafeAreaView>
+
   );
 };
 
